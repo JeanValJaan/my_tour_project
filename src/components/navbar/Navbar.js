16e3db88a4dcb5895673aadb42a09bf187277fde
Jean Val Jaan <jeanvaljaan@gmail.com>
2021-05-02 23:37:05 +0300

    created my tour app using react

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <a href="about us">ABOUT</a>
+            <a href="foryou">FORYOU</a>
+            <a href="services">SERVICES</a>
+            <a href="blog">BLOG</a>
+            <a href="vlog">VLOG</a>
+            <a href="contact">CONTACT</a>
+        </div>
+    )
+    
+    }
+export default Navbar;
